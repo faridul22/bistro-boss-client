4c1617c139d1e43e38b9e329a60c88915ad0ce61
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-05-26 10:33:31 +0600

    navbar, banner slider, swiper and footer added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Bistro Boss Restaurant</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import slider1 from "../../../assets/home/slide1.jpg"
+import slider2 from "../../../assets/home/slide2.jpg"
+import slider3 from "../../../assets/home/slide3.jpg"
+import slider4 from "../../../assets/home/slide4.jpg"
+import slider5 from "../../../assets/home/slide5.jpg"
+
+const Category = () => {
+    return (
+        <div>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-20"
+            >
+                <SwiperSlide>
+                    <img src={slider1} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-20 text-white">Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider2} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-20 text-white">Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider3} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-20 text-white">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider4} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-20 text-white">Desserts</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider5} alt="" />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
