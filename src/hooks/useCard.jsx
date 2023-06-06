dadb989c7a266c6c7c07e9e2b08f637ac1588cf4
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-06-06 10:36:52 +0600

    axios instance use and created custom hooks useAuth and useAxiosSecure

--- src/hooks/useCard.jsx
+++ src/hooks/useCard.jsx
@@ -1,14 +1,26 @@
-import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
 
 const useCard = () => {
-    const { user } = useContext(AuthContext)
+    const { user } = useAuth();
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
+
     const { isLoading, refetch, data: card = [] } = useQuery({
         queryKey: ['cards', user?.email],
+        // queryFn: async () => {
+        //     const response = await fetch(`http://localhost:5000/cards?email=${user?.email}`, {
+        //         headers: {
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return response.json()
+        // },
+
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/cards?email=${user?.email}`)
-            return response.json()
+            const res = await axiosSecure(`/cards?email=${user?.email}`)
+            return res.data
         },
     })
     return [card, refetch, isLoading]
